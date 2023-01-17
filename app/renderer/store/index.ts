5dcb7bc30887b26a363f6d6299b61a9201e76e40
Marc <markxvii@outlook.com>
2023-01-17 23:07:34 +0800

    简历边栏各项的添加和删除，点击已添加项弹出表单dialog，事件模型封装

--- app/renderer/store/index.ts
+++ app/renderer/store/index.ts
@@ -1,13 +1,19 @@
+// app/renderer/store/index.ts
 // @ts-ignore
 import logger from 'redux-logger';
 // @ts-ignore
 import RcReduxModel from 'rc-redux-model';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-// @ts-ignore
+
+// 引入写好的 model
 import globalModel from './globalModel';
+import resumeModel from './resumeModel';
+import templateModel from './templateModel';
 
-const reduxModel = new RcReduxModel([globalModel]);
+// 这里需要调用 RcReduxModel 实例化一下得到最后的 reduxModel
+const reduxModel = new RcReduxModel([globalModel, resumeModel, templateModel]);
 
+// 无侵入式的使用 Redux，即使写最原始的 reducer 也照样支持
 const reducerList = combineReducers(reduxModel.reducers);
 
 export default createStore(reducerList, applyMiddleware(reduxModel.thunk, logger));
